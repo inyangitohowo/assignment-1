55a05395bc450e5dc8618ad881771fc44a56553a
Inyangitohow <Inyangitohowo@yahoo.com>
2015-05-21 13:48:56 -0400

    my assigment on cpp
    cpp assignment

---
+++ number 3.cpp
@@ -0,0 +1,23 @@
+/*
+* number3.cpp
+* Name: [Inyang itohowo polycarp]
+* Department: [Computer Science]
+* Matric No: [45725633BG]
+* A Program that print a box,oval,arrow and a diamond 
+*
+* Stub file to enable you complete assignment#1-question#3
+*/
+#include <iostream>
+
+int main(){
+	std::cout << "\n*********        ***           *             *";
+	std::cout << "\n*       *     *       *       ***           * *";
+	std::cout << "\n*       *    *         *     *****         *   *";
+	std::cout << "\n*       *    *         *       *          *     *";
+	std::cout << "\n*       *    *         *       *         *       *";
+	std::cout << "\n*       *    *         *       *          *     *";
+	std::cout << "\n*       *    *         *       *           *   *";
+	std::cout << "\n*       *     *       *        *            * *";
+	std::cout << "\n*********        ***           *             *";
+}
+
